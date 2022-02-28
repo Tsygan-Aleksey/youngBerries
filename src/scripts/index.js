28a34c389fe05306601b0452d7ef162ec48ab014
Tsygan-Aleksey <newrety1996@gmail.com>
2022-02-28 21:05:24 +0300

    fix conflict in index.js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,12 @@
-
-import { addProduct } from "./templates/templates.js";
-=======
-import { containerCard, createCard } from "./templates/templates.js";
-import { CATALOG } from "./data/data.js";
-import {
-  onModalBasket,
-  onDeleteBasket,
-  basketModalPriceSum,
-} from "./components/basket.js";
-import {
-  createBasketCard,
-  createBigCard,
-} from "./components/quick-view-modal-window.js";
 import {
   searchCard
 } from "./components/search.js";
+import { addProduct } from "./templates/templates.js";
+
 const search = document.querySelector('.header__input')
-search.oninput = searchCard
-
-
 const containerCard = document.querySelector("#container-card");
+
+search.oninput = searchCard
 addProduct();
 
 export { containerCard };
