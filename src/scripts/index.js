be020743e84d8e6e9992e39ebfaaf7b1faa557f4
Tsygan-Aleksey <94075936+Tsygan-Aleksey@users.noreply.github.com>
2022-02-28 19:56:25 +0300

    Update index.js
    fix conflict

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,23 @@
 
+import { addProduct } from "./templates/templates.js";
+=======
+import { containerCard, createCard } from "./templates/templates.js";
+import { CATALOG } from "./data/data.js";
+import {
+  onModalBasket,
+  onDeleteBasket,
+  basketModalPriceSum,
+} from "./components/basket.js";
+import {
+  createBasketCard,
+  createBigCard,
+} from "./components/quick-view-modal-window.js";
+import {
+  searchCard
+} from "./components/search.js";
+const search = document.querySelector('.header__input')
+search.oninput = searchCard
+
 
 const containerCard = document.querySelector("#container-card");
 addProduct();
